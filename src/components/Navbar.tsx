743684d13e0690bda5a21b263fbcb8e7ee28f253
Fsmedia <71898347+SojiJoro@users.noreply.github.com>
2025-06-17 09:04:21 +0100

    Use Next.js Link for internal navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 export default function Navbar() {
   return (
     <nav className="p-4 border-b flex gap-4">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/resume">Resume</a>
-      <a href="/projects">Projects</a>
-      <a href="/contact">Contact</a>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/resume">Resume</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/contact">Contact</Link>
     </nav>
   )
 }
