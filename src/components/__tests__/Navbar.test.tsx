c1bfdc193073bd2495c030171ec2ba091fd11250
Fsmedia <71898347+SojiJoro@users.noreply.github.com>
2025-06-20 11:50:24 +0100

    Make navbar logo link to homepage

--- src/components/__tests__/Navbar.test.tsx
+++ src/components/__tests__/Navbar.test.tsx
@@ -4,6 +4,7 @@ import Navbar from '../Navbar'
 describe('Navbar', () => {
   it('renders all navigation links', () => {
     render(<Navbar />)
+    expect(screen.getByRole('link', { name: /adesoji/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /resume/i })).toBeInTheDocument()
